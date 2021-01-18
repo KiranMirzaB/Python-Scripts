4952efe27d34b3143f09f65c8b68f5426c82a343
KiranMirzaB <kiran.mirza.b@gmail.com>
2021-01-18 20:51:24 +0500

    first commit

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+##(in python use # and ''' to comment)
+message="hello"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
+
+abc=25
+print(abc)
+
+xender = "ncbvmxncbvn"
+print(xender.title())
+
+chuchu = "kookachu"
+print (chuchu.upper())
+
+pickachu = "BLOB"
+print (pickachu.lower())
+
+name="mirza"
+fst_name="ubaid"
+mid_name="ullah"
+lst_name="BAIG,"
+msg="i'm done for today :D"
+print(name.title()+" "+fst_name.upper()+" "+mid_name.title()+" "+lst_name.lower()+" "+msg)
+
---
+++ qoutes.py
@@ -0,0 +1,15 @@
+##name="Kim Namjoon:"
+##qut='"if you can\'t respect, don\'t even open your mouth.\n\tif you can\'t respect, don\'t even open your mouth"'
+##print("\n\t"+name.title()+qut)
+
+msg=" \tPrint the name once,so the whitespace\n\taround the name is displayed "
+print("normal msg"+msg)
+print("\n")
+print("left strip",msg.lstrip())
+print("\n")
+print("right strip"+msg.rstrip())
+print("\n")
+print("strip"+msg.strip())
+
+
+
